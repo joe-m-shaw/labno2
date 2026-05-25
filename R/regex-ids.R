2726b0b10b87a36d738274a6777f862b5aff9f54
joeshaw824 <57156322+joeshaw824@users.noreply.github.com>
2026-05-25 06:49:05 +0100

    Add package structure

---
+++ R/regex-ids.R
@@ -0,0 +1,18 @@
+regex_ids <- function(){
+
+  #' Regular expressions for sample identifiers
+  #'
+  #' @returns A named list of regular expressions for different identifiers
+  #' @export
+  #'
+  #' @examples regex_ids()$labno
+
+  output_list <- list(
+
+    "worksheet" = "(WS\\d{6})",
+    "labno_suffix" = "(\\d{8})(a|b|c|d|)"
+  )
+
+  return(output_list)
+
+}
